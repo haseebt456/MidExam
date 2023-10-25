45e1f8a8ca32013afea5f1445122a3d07dd644cc
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-10-25 15:36:46 +0500

    card adding functionaly added

---
+++ index.js
@@ -0,0 +1,21 @@
+function Add()
+{
+    var question = document.getElementById("query").value;
+    var answer = document.getElementById("ans").value;
+    const cont = document.getElementById('container');
+    const card = document.createElement('div');
+    const queElement = document.createElement('h3');
+    const ansElement = document.createElement('p');
+    const arrow = document.createElement('button');
+    arrow.setAttribute("class","faq-toggle");
+    arrow.innerHTML =' <i class="fas fa-chevron-down"></i><i class="fas fa-times"></i>'; 
+    queElement.innerText = question;
+    ansElement.innerText = answer;
+    queElement.setAttribute('class','faq-title');
+    ansElement.setAttribute('class','faq-text');
+    card.appendChild(queElement);
+    card.appendChild(ansElement);
+    card.appendChild(arrow);
+    card.setAttribute("class","faq");
+    cont.appendChild(card);
+}
